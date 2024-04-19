0e0973099b56b5b86d2332b2385fa052b9a1cc8f
Jalil <jalil@jalils-mbp.wkstn.qmul.ac.uk>
2024-04-19 12:18:30 +0100

    updated .CALL and C code correctly

--- R/C_calls.R
+++ R/C_calls.R
@@ -3,7 +3,7 @@
 mufC<-function(v) {
   n<-length(v)
   x<-rep(0,n*(n+1)/2)
-  out<-.C("muf",v=as.double(v),x=as.double(x),n=as.integer(n),PACKAGE="FIRMAGene")
+  out<-.Call("muf",v=as.double(v),x=as.double(x),n=as.integer(n),PACKAGE="FIRMAGene")
   out
 }
 
