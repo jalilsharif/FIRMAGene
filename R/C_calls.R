92812f3e589fe588d452a720032af5311128f4d7
Jalil <jalil@jalils-mbp.wkstn.qmul.ac.uk>
2024-04-19 12:17:22 +0100

    reverted to original code

--- R/C_calls.R
+++ R/C_calls.R
@@ -3,7 +3,7 @@
 mufC<-function(v) {
   n<-length(v)
   x<-rep(0,n*(n+1)/2)
-  out<-.Call("muf",v=as.double(v),x=as.double(x),n=as.integer(n),PACKAGE="FIRMAGene")
+  out<-.C("muf",v=as.double(v),x=as.double(x),n=as.integer(n),PACKAGE="FIRMAGene")
   out
 }
 
--- src/mps.c
+++ src/mps.c
@@ -2,22 +2,31 @@
 #include <stdio.h>
 #include <math.h>
 
-oid muf(double *v, double *x, long *n_) {
-    int i, j, k, n = *n_, count = 0;
-    double sum = 0., *cumsum = malloc((n + 1) * sizeof(double));
-    cumsum[0] = 0.; // Initialize cumsum
+void muf(double *v, double *x, long *n_)
+{
 
-    // Compute cumulative sums
-    for (i = 1; i <= n; i++) {
-        cumsum[i] = cumsum[i - 1] + v[i - 1];
+  int i, j, k, n = *n_, count=0;
+  double sum=0.;
+  
+  /*
+  printf("%d\n",n);
+  for (i=0; i< n; i++)
+    printf("v[%d]=%f\n",i,v[i]);
+  for (i=0; i< n*(n+1)/2; i++)
+    printf("x[%d]=%f\n",i,x[i]);
+  */
+  
+  for (i=0; i< n; i++)
+    for (j=i; j<n; j++) {
+       sum=0.;
+       for (k=i; k<=j; k++) {
+         sum+=v[k];
+         /* printf("i=%d, j=%d, k=%d, sum=%f\n",i,j,k,sum); */
+       }
+       x[count]=sum/sqrt(j-i+1.);
+       /* printf("(denom=%d) x[%d]=%f\n",(j-i+1),count,x[count]); */
+       count+=1;
     }
-
-    for (i = 0; i < n; i++) {
-        for (j = i; j < n; j++) {
-            sum = cumsum[j + 1] - cumsum[i];
-            x[count++] = sum / sqrt(j - i + 1.);
-        }
-    }
-
-    free(cumsum); // Clean up the allocated memory
+   
 }
+   
